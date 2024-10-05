2464928833bde702e5cb15f7f946b77b97b82459
FelipeFidanza <ffidanzar@gmail.com>
2024-10-04 23:38:25 -0300

    Mostrar la info de todas las tablas a través de @api_view(['GET'])

--- webScrapping/urls.py
+++ webScrapping/urls.py
@@ -4,14 +4,15 @@ from .views import *
 
 router = routers.DefaultRouter()
 
-router.register(r'api/news', NewsViewSet, basename='news')
-router.register(r'api/auctions', AuctionsViewSet, basename='auctions')
-router.register(r'api/artists', ArtistsViewSet, basename='artists')
+#router.register(r'api/news', NewsViewSet, basename='news')
+#router.register(r'api/auctions', AuctionsViewSet, basename='auctions')
+#router.register(r'api/artists', ArtistsViewSet, basename='artists')
 
 #urlpatterns = router.urls
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api/verNoticias', viewNews, name='Noticias')
+    path('api/verNoticias', viewNews, name='Noticias'),
+    path('api/verSubastas', viewAuctions, name='Subastas'),
+    path('api/verArtistas', viewArtists, name='Artistas'),
 ]
-
--- webScrapping/views.py
+++ webScrapping/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-
-
 from .models import *
 from rest_framework import viewsets, permissions
 from .serializers import *
@@ -12,27 +9,38 @@ from .serializers import *
 
 # Create your views here.
 
-class NewsViewSet(viewsets.ModelViewSet):
-    queryset = News.objects.all()   # pylint: disable=no-member
-    serializer_class = NewsSerializer
-    permissions_classes = [permissions.AllowAny]
+# class NewsViewSet(viewsets.ModelViewSet):
+#     queryset = News.objects.all()   # pylint: disable=no-member
+#     serializer_class = NewsSerializer
+#     permissions_classes = [permissions.AllowAny]
 
-class AuctionsViewSet(viewsets.ModelViewSet):
-    queryset = Auctions.objects.all()   # pylint: disable=no-member
-    serializer_class = AuctionsSerializer
-    permissions_classes = [permissions.AllowAny]
+# class AuctionsViewSet(viewsets.ModelViewSet):
+#     queryset = Auctions.objects.all()   # pylint: disable=no-member
+#     serializer_class = AuctionsSerializer
+#     permissions_classes = [permissions.AllowAny]
 
-class ArtistsViewSet(viewsets.ModelViewSet):
-    queryset = Artists.objects.all()   # pylint: disable=no-member
-    serializer_class = ArtistsSerializer
-    permissions_classes = [permissions.AllowAny]
-
-
-
-
+# class ArtistsViewSet(viewsets.ModelViewSet):
+#     queryset = Artists.objects.all()   # pylint: disable=no-member
+#     serializer_class = ArtistsSerializer
+#     permissions_classes = [permissions.AllowAny]
 
 
 @api_view(['GET'])
 def viewNews(request):
+    news = News.objects.all() # pylint: disable=no-member
+    serializer = NewsSerializer(news, many = True)
+    return Response(serializer.data)
 
-    return Response({})
+
+@api_view(['GET'])
+def viewAuctions(request):
+    auctions = Auctions.objects.all() # pylint: disable=no-member
+    serializer = AuctionsSerializer(auctions, many = True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def viewArtists(request):
+    artists = Artists.objects.all() # pylint: disable=no-member
+    serializer = ArtistsSerializer(artists, many = True)
+    return Response(serializer.data)
