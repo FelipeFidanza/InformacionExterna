279e4d5862dfdb9c56d4098105e630889c475d00
FelipeFidanza <ffidanzar@gmail.com>
2024-12-02 13:24:02 -0300

    Listo para render 2.0

--- webScrapping/scrappers/news.py
+++ webScrapping/scrappers/news.py
@@ -1,9 +1,6 @@
 import sys
 import os
 import django
-from webScrapping.models import News
-from .bienalVenezia import bienalVenezia
-from .infoDesigners import infoDesigners
 
 # Agrega el directorio raíz del proyecto al sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
@@ -15,23 +12,34 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InfoExterna.settings')
 django.setup()
 
 
+from webScrapping.models import News
+#from webScrapping.scrappers.bienalVenezia import bienalVenezia #pylint: disable=no-member
+from webScrapping.scrappers.infoDesigners import infoDesigners #pylint: disable=no-member
+
+
+
 def getNews():
 
     # Llamo a las funciones de los archivos de noticias
-    dicBienalVenezia = bienalVenezia()
+    #dicBienalVenezia = bienalVenezia()
     dicInfoDesigners = infoDesigners()
 
     # Creo listas con la información de cada atributo de las noticias
-    newsTitles = dicBienalVenezia["titles"]
-    newsDescriptions = dicBienalVenezia["descriptions"]
-    newsImages = dicBienalVenezia["images"]
-    newsUrls = dicBienalVenezia["urls"]
+    newsTitles = dicInfoDesigners["titles"]
+    newsDescriptions = dicInfoDesigners["descriptions"]
+    newsImages = dicInfoDesigners["images"]
+    newsUrls = dicInfoDesigners["urls"]
+
+    #newsTitles = dicBienalVenezia["titles"]
+    #newsDescriptions = dicBienalVenezia["descriptions"]
+    #newsImages = dicBienalVenezia["images"]
+    #newsUrls = dicBienalVenezia["urls"]
 
     # Agrego todas las noticias a las listas
-    newsTitles.extend(dicInfoDesigners["titles"])
-    newsDescriptions.extend(dicInfoDesigners["descriptions"])
-    newsImages.extend(dicInfoDesigners["images"])
-    newsUrls.extend(dicInfoDesigners["urls"])
+    #newsTitles.extend(dicInfoDesigners["titles"])
+    #newsDescriptions.extend(dicInfoDesigners["descriptions"])
+    #newsImages.extend(dicInfoDesigners["images"])
+    #newsUrls.extend(dicInfoDesigners["urls"])
     
     # Cargo las noticias a la base de datos
     for i, title in enumerate(newsTitles):
