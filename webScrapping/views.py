7eaeaadc6422cc494a6dde78f80d02a2f4ad7755
FelipeFidanza <ffidanzar@gmail.com>
2024-12-03 15:58:18 -0300

    Permite método POST

--- webScrapping/views.py
+++ webScrapping/views.py
@@ -3,44 +3,44 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
-# from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions
 from .serializers import *
 
 
 # Create your views here.
 
-# class NewsViewSet(viewsets.ModelViewSet):
-#     queryset = News.objects.all()   # pylint: disable=no-member
-#     serializer_class = NewsSerializer
-#     permissions_classes = [permissions.AllowAny]
+class NewsViewSet(viewsets.ModelViewSet):
+    queryset = News.objects.all()   # pylint: disable=no-member
+    serializer_class = NewsSerializer
+    permissions_classes = [permissions.AllowAny]
 
-# class AuctionsViewSet(viewsets.ModelViewSet):
-#     queryset = Auctions.objects.all()   # pylint: disable=no-member
-#     serializer_class = AuctionsSerializer
-#     permissions_classes = [permissions.AllowAny]
+class AuctionsViewSet(viewsets.ModelViewSet):
+    queryset = Auctions.objects.all()   # pylint: disable=no-member
+    serializer_class = AuctionsSerializer
+    permissions_classes = [permissions.AllowAny]
 
-# class ArtistsViewSet(viewsets.ModelViewSet):
-#     queryset = Artists.objects.all()   # pylint: disable=no-member
-#     serializer_class = ArtistsSerializer
-#     permissions_classes = [permissions.AllowAny]
-
-
-@api_view(['GET'])
-def viewNews(request):
-    news = News.objects.all() # pylint: disable=no-member
-    serializer = NewsSerializer(news, many = True)
-    return Response(serializer.data)
+class ArtistsViewSet(viewsets.ModelViewSet):
+    queryset = Artists.objects.all()   # pylint: disable=no-member
+    serializer_class = ArtistsSerializer
+    permissions_classes = [permissions.AllowAny]
 
 
-@api_view(['GET'])
-def viewAuctions(request):
-    auctions = Auctions.objects.all() # pylint: disable=no-member
-    serializer = AuctionsSerializer(auctions, many = True)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def viewNews(request):
+#     news = News.objects.all() # pylint: disable=no-member
+#     serializer = NewsSerializer(news, many = True)
+#     return Response(serializer.data)
 
 
-@api_view(['GET'])
-def viewArtists(request):
-    artists = Artists.objects.all() # pylint: disable=no-member
-    serializer = ArtistsSerializer(artists, many = True)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def viewAuctions(request):
+#     auctions = Auctions.objects.all() # pylint: disable=no-member
+#     serializer = AuctionsSerializer(auctions, many = True)
+#     return Response(serializer.data)
+
+
+# @api_view(['GET'])
+# def viewArtists(request):
+#     artists = Artists.objects.all() # pylint: disable=no-member
+#     serializer = ArtistsSerializer(artists, many = True)
+#     return Response(serializer.data)
